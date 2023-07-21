6dadfbd68b9b0800d74c215ae4c512ea6166bd81
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-07-21 15:23:57 -0400

    finishing foxy

--- src/init.js
+++ src/init.js
@@ -1,25 +1,22 @@
-import gameState from "./gameState";
-import { TICK_RATE } from "./constants";
+import game, { handleUserAction } from "./gameState";
 import initButtons from "./buttons";
+import { TICK_RATE } from "./constants";
 
-function init() {
+async function init() {
   console.log("starting game");
-  initButtons(gameState.handleUserAction);
+  initButtons(handleUserAction);
 
   let nextTimeToTick = Date.now();
-
   function nextAnimationFrame() {
     const now = Date.now();
-
     if (nextTimeToTick <= now) {
-      gameState.tick();
+      game.tick();
       nextTimeToTick = now + TICK_RATE;
     }
-
     requestAnimationFrame(nextAnimationFrame);
   }
 
-  requestAnimationFrame(nextAnimationFrame);
+  nextAnimationFrame();
 }
 
 init();
